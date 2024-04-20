4bd27a77b95bff07b99beda1e1a823d3b097f38f
Turu0512 <hinataptyan@gmail.com>
2024-04-21 08:28:24 +0900

    元の実装に戻す

--- src/api/fetchWaveData.ts
+++ src/api/fetchWaveData.ts
@@ -1,6 +1,6 @@
 import { WaveData } from '../types/wave';
 import { ErrorCode } from '../types/error';
-import { fetchWeatherApi } from 'openmeteo';
+// import { fetchWeatherApi } from 'openmeteo';
 
 const point = 'POINT1'; // これは選択または入力によって動的に変更されることがあります。
 const coords = process.env[`REACT_APP_${point}_COORDS`] ?? '000,000';
@@ -8,28 +8,28 @@ console.log(coords)
 
 const [latitude, longitude] = coords.split(',');
 
-const params = {
-  "latitude": latitude,
-  "longitude": longitude,
-  "current": "swell_wave_height",
-  "hourly": [
-    "wave_height",
-    "wave_direction",
-    "wind_wave_height",
-    "wind_wave_direction",
-    "swell_wave_height",
-    "swell_wave_direction"
-  ],
-  "timezone": "Asia/Tokyo"
-};
-const url = "https://marine-api.open-meteo.com/v1/marine";
+// const params = {
+//   "latitude": latitude,
+//   "longitude": longitude,
+//   "current": "swell_wave_height",
+//   "hourly": [
+//     "wave_height",
+//     "wave_direction",
+//     "wind_wave_height",
+//     "wind_wave_direction",
+//     "swell_wave_height",
+//     "swell_wave_direction"
+//   ],
+//   "timezone": "Asia/Tokyo"
+// };
+// const url = "https://marine-api.open-meteo.com/v1/marine";
 
 const KOIGAURA_API_URL = 'https://marine-api.open-meteo.com/v1/marine?latitude=31.412205&longitude=131.343669&current=swell_wave_height&hourly=wave_height,wave_direction,wind_wave_height,wind_wave_direction,swell_wave_height,swell_wave_direction&timezone=Asia%2FTokyo';
 
 export const fetchWaveData = async (): Promise<WaveData | ErrorCode> => {
   if (longitude === '000') {return { error: true, code: 'INVALID_LONGITUDE', message: 'Invalid longitude' };  }
   try {
-    const responses = await fetchWeatherApi(url, params);
+    // const responses = await fetchWeatherApi(url, params);
 
     const response = await fetch(`${KOIGAURA_API_URL}`);
     if (!response.ok) {
@@ -40,6 +40,6 @@ export const fetchWaveData = async (): Promise<WaveData | ErrorCode> => {
     return waveData;
   } catch (error) {
     console.error('There was a problem with the fetch operation:', error);
-    return { error: true, code: 'FETCH_ERROR', message: `There was a problem with the fetch operation: ${error}` };
+    throw error;
   }
 };
