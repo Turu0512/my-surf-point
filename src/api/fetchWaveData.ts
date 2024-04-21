376d2c3820861d0582977c7a5549bd7f0c71675c
Turu0512 <hinataptyan@gmail.com>
2024-04-22 07:34:54 +0900

    URLを動的に生成

--- src/api/fetchWaveData.ts
+++ src/api/fetchWaveData.ts
@@ -7,12 +7,24 @@ console.log(coords)
 
 const [latitude, longitude] = coords.split(',');
 
-const KOIGAURA_API_URL = 'https://marine-api.open-meteo.com/v1/marine?latitude=31.412205&longitude=131.343669&current=swell_wave_height&hourly=wave_height,wave_direction,wind_wave_height,wind_wave_direction,swell_wave_height,swell_wave_direction&timezone=Asia%2FTokyo';
+const baseUrl = "https://marine-api.open-meteo.com/v1/marine";
+
+const params = {
+  latitude: latitude,
+  longitude: longitude,
+  current: "swell_wave_height",
+  hourly: ["wave_height", "wave_direction", "wind_wave_height", "wind_wave_direction", "swell_wave_height", "swell_wave_direction"],
+  timezone: "Asia%2FTokyo"
+};
+
+const point_api_url = `${baseUrl}?latitude=${params.latitude}&longitude=${params.longitude}&current=${params.current}&hourly=${params.hourly.join(',')}&timezone=${params.timezone}`;
+
+console.log(point_api_url);
 
 export const fetchWaveData = async (): Promise<WaveData | ErrorCode> => {
   if (longitude === '000') {return { error: true, code: 'INVALID_LONGITUDE', message: 'Invalid longitude' };  }
   try {
-    const response = await fetch(`${KOIGAURA_API_URL}`);
+    const response = await fetch(`${point_api_url}`);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
