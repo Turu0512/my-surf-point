2ecb076e256354f1be100755d7583de65a739f0b
Turu0512 <hinataptyan@gmail.com>
2024-04-27 08:06:11 +0900

    ドロップダウンメニューの実装

--- src/components/dashboard/listItems.tsx
+++ src/components/dashboard/listItems.tsx
@@ -1,16 +1,39 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ListSubheader from '@mui/material/ListSubheader';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 
-export const mainListItems = (
+
+export default function MainListItems() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => { // Changed here
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const points = [
+    { id: "koigaura", name: process.env['REACT_APP_KOIGAURA_POINT'] },
+    { id: "nishikata", name: process.env['REACT_APP_NISHIKATA_POINT'] },
+];
+
+return (
+
   <React.Fragment>
     <ListItemButton>
       <ListItemIcon>
@@ -18,18 +41,25 @@ export const mainListItems = (
       </ListItemIcon>
       <ListItemText primary="Dashboard" />
     </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="ここ" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Customers" />
-    </ListItemButton>
+    <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <ShoppingCartIcon />
+        </ListItemIcon>
+        <ListItemText primary="ポイント一覧" />
+      </ListItemButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {points.map((point) => (
+          <MenuItem key={point.id} onClick={handleClose} component={Link} to={`/points/${point.id}`}>
+              {point.name}
+          </MenuItem>
+        ))}
+      </Menu>
     <ListItemButton>
       <ListItemIcon>
         <BarChartIcon />
@@ -44,6 +74,7 @@ export const mainListItems = (
     </ListItemButton>
   </React.Fragment>
 );
+}
 
 export const secondaryListItems = (
   <React.Fragment>
