99e6729c4452d7cafaaa0cd05bd3fa7a4359a61e
Turu0512 <hinataptyan@gmail.com>
2024-05-06 07:47:02 +0900

    画像の表示サイズを変更

--- src/components/dashboard/Chart.tsx
+++ src/components/dashboard/Chart.tsx
@@ -8,11 +8,11 @@ import Title from './Title';
 export default function Chart() {
   return (
     <React.Fragment>
-      <Title>Today</Title>
-      <div style={{ width: '100%', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <img style={{ maxWidth: '100%', height: 'auto', objectFit: 'contain' }} src="https://api.tide736.net/tide_image.php?pc=28&hc=9&yr=2024&mn=5&dy=2&rg=day&w=768&h=512&lc=blue&gcs=cyan&gcf=blue&ld=on&ttd=on&tsmd=on" alt="Tide chart" />
+      <div style={{ width: '100%', height: '100%', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <img style={{ height: '100%', objectFit: 'contain' }} src="https://api.tide736.net/tide_image.php?pc=28&hc=9&yr=2024&mn=5&dy=2&rg=day&w=768&h=512&lc=blue&gcs=cyan&gcf=blue&ld=on&ttd=on&tsmd=on" alt="Tide chart" />
       </div>
     </React.Fragment>
   );
 }
+
 
