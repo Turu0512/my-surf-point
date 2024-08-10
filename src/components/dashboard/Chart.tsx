d1355c158170642d282f352e7ab066ec38e7c151
Turu0512 <hinataptyan@gmail.com>
2024-08-11 07:51:50 +0900

    画像を出力するように修正

--- src/components/dashboard/Chart.tsx
+++ src/components/dashboard/Chart.tsx
@@ -1,18 +1,31 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import { LineChart, axisClasses } from '@mui/x-charts';
-import { ChartsTextStyle } from '@mui/x-charts/ChartsText';
-import Title from './Title';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
+import { fetchTideData } from '../../api/fetchTideData';
 
 export default function Chart() {
+  const { point = 'POINT1' } = useParams<{ point?: string }>();
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const day = currentDate.getDate();
+    const pc = process.env[`REACT_APP_${point.toUpperCase()}_PC`] || '28';
+    const hc = process.env[`REACT_APP_${point.toUpperCase()}_HC`] || '9';
+    const data = fetchTideData(year, month, day);
+    // setTideData(data)
+
+    const url = `https://api.tide736.net/tide_image.php?pc=${pc}&hc=${hc}&yr=${year}&mn=${month}&dy=${day}&rg=day&w=768&h=512&lc=blue&gcs=cyan&gcf=blue&ld=on&ttd=on&tsmd=on`;
+    setImageUrl(url);
+  }, [point]);
+
   return (
     <React.Fragment>
       <div style={{ width: '100%', height: '100%', overflow: 'hidden', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <img style={{ height: '100%', objectFit: 'contain' }} src="https://api.tide736.net/tide_image.php?pc=28&hc=9&yr=2024&mn=5&dy=2&rg=day&w=768&h=512&lc=blue&gcs=cyan&gcf=blue&ld=on&ttd=on&tsmd=on" alt="Tide chart" />
+        <img style={{ height: '100%', objectFit: 'contain' }} src={imageUrl} alt="潮汐チャート" />
       </div>
     </React.Fragment>
   );
 }
-
-
